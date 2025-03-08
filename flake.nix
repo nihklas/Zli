42fcd4e81c3d227de8f0c66425a3d8cfb6a091a8
Niklas Koll <niklas@nkoll.de>
2025-03-08 11:08:21 +0100

    Update zig 0.14.0, update flake

--- flake.nix
+++ flake.nix
@@ -1,43 +1,45 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    flake-utils.url = "github:numtide/flake-utils";
-    zig.url = "github:mitchellh/zig-overlay";
-    zigscient.url = "gitlab:nihklas/zigscient-nix";
+
+    zig-overlay.url = "github:mitchellh/zig-overlay";
+    zig-overlay.inputs.nixpkgs.follows = "nixpkgs";
+
+    zls.url = "github:zigtools/zls/0.14.0";
+    zls.inputs.nixpkgs.follows = "nixpkgs";
+    zls.inputs.zig-overlay.follows = "zig-overlay";
   };
 
   outputs = {
-    self,
     nixpkgs,
-    flake-utils,
+    zig-overlay,
+    zls,
     ...
-  } @ inputs:
-    flake-utils.lib.eachDefaultSystem
-    (
-      system: let
-        overlays = [
-          inputs.zig.overlays.default
-          inputs.zigscient.overlays.default
+  }: let
+    systems = ["aarch64-darwin" "x86_64-linux"];
+    eachSystem = function:
+      nixpkgs.lib.genAttrs systems (system:
+        function {
+          inherit system;
+          target = builtins.replaceStrings ["darwin"] ["macos"] system;
+          pkgs = nixpkgs.legacyPackages.${system};
+          zig = zig-overlay.packages.${system}.master;
+        });
+  in {
+    devShells = eachSystem ({
+      system,
+      pkgs,
+      zig,
+      ...
+    }: {
+      default = pkgs.mkShellNoCC {
+        packages = [
+          pkgs.lua-language-server
+          zls.packages.${system}.default
+          zig
         ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-      in
-        with pkgs; {
-          devShells.default = mkShell {
-            packages = [
-              zigpkgs.master
-              zigscient
-            ];
-            shellHook = ''
-              # We unset some NIX environment variables that might interfere with the zig compiler.
-              # Issue: https://github.com/ziglang/zig/issues/18998
-              unset NIX_CFLAGS_COMPILE
-              unset NIX_LDFLAGS
-              printf '\n'
-              echo "Running Zig Version: $(zig version)"
-            '';
-          };
-        }
-    );
+      };
+    });
+  };
 }
+
